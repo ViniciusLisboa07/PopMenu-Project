c6b30e933e1f308fe19a907a439262bcde6a7f53
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2025-04-18 19:34:06 -0300

    feat: add MenuItem model with validations and associations, update Menu model, and create corresponding database migration and fixtures

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_18_221838) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_18_222851) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "menu_items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.bigint "menu_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["menu_id"], name: "index_menu_items_on_menu_id"
+  end
 
   create_table "menus", force: :cascade do |t|
     t.string "name"
@@ -22,4 +32,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_18_221838) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "menu_items", "menus"
 end
